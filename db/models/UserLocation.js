002c43fdbceb942d8e8019d7d2bdd0d55ea21e5d
Aman0246jploft <aman_kashyap@jploft.in>
2025-06-30 13:18:42 +0530

    save

---
+++ db/models/UserLocation.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// user Address
+
+const UserlocationSchema = new Schema({
+    userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    line1: { type: String, required: true },
+    country: { type: String, required: true },
+    state: { type: String, required: true },
+    city: { type: String, required: true },
+    postalCode: { type: String, required: true, trim: true },
+    isActive: { type: Boolean, default: false },
+    isDisable: { type: Boolean, default: false },
+    isDeleted: { type: Boolean, default: false },
+}, {
+    timestamps: true
+});
+
+UserlocationSchema.index({ userId: 1, isDefault: 1 });
+
+module.exports = mongoose.model("UserLocation", UserlocationSchema, 'UserLocation');
