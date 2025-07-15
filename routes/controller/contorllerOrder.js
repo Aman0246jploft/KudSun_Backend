14c76d30b2d39d82b8911e71a997b8fc88fa539a
Aman0246jploft <aman_kashyap@jploft.in>
2025-07-15 18:18:44 +0530

    save

--- routes/controller/contorllerOrder.js
+++ routes/controller/contorllerOrder.js
@@ -860,6 +860,8 @@ const getBoughtProducts = async (req, res) => {
                 order.labalStatuses = 'Unpaid';
                 order.allowedNextStatuses = 'Pay now';
             } else if (!order.isReviewed) {
+                order.labalStatuses = 'Unreviewed';
+
                 if (order.status == ORDER_STATUS.SHIPPED) {
                     order.labalStatuses = 'Shipped';
 
