da326c0aed1d9a460a522efac0a0e552363b8810
Aman0246jploft <amakashyap0246jploft@gmail.com>
2025-08-28 16:31:17 +0530

    Save

--- config/socket.js
+++ config/socket.js
@@ -606,6 +606,7 @@ async function setupSocket(server) {
 
           // Optional: reverse to send oldest first
           messages = messages.reverse();
+          socket.emit("markRoomMessagesAsRead", { otherUserId });
 
           socket.emit("messageList", {
             chatRoomId,
