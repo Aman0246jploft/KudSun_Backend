8b38ea4a53183cacf80c878f0a4bbca514c3433d
Aman0246jploft <amankashyap0246jploft@gmail.com>
2025-09-10 17:44:48 +0530

    save

--- script/adminsetup.js
+++ script/adminsetup.js
@@ -2,14 +2,38 @@ require('dotenv').config();
 const { User } = require('../db');
 const { roleId } = require('../utils/Role');
 
+// adminCreate = async () => {
+//     let data = {
+//         userName: "Superadmin",
+//         email: "superadmin@gmail.com",
+//         password: "123456",
+//         phoneNumber: "1234567890",
+//         profileImage: null,
+//         roleId: roleId.SUPER_ADMIN,
+//     }
+//     try {
+//         let testAdmin = new User(data);
+//         let res = await testAdmin.save();
+//         console.log("res ", res);
+//         process.exit(1)
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+// (async () => {
+//     await adminCreate();
+// })();
+
+
+
 adminCreate = async () => {
     let data = {
-        userName: "Superadmin",
-        email: "superadmin@gmail.com",
-        password: "123456",
-        phoneNumber: "1234567890",
+        userName: "GuestXYZ",
+        email: "guestxyz@gmail11.com",
+        password: "0000000011",
+        phoneNumber: "0000000011",
         profileImage: null,
-        roleId: roleId.SUPER_ADMIN,
+        roleId: roleId.GUEST,
     }
     try {
         let testAdmin = new User(data);
